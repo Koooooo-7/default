9a965bdb2d90359544002e9bb6f0b78b94666552
koooooo-7 <369491420@qq.com>
2020-04-30 22:44:17 +0800

    [init gateway]

---
+++ kono_gateway/src/main/java/com/koy/kono_gateway/KonoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.koy.kono_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonoGatewayApplication.class, args);
+    }
+
+}
---
+++ kono_gateway/src/test/java/com/koy/kono_gateway/KonoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koy.kono_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
